697c44bbf7a41f5d5dcf4a6d29dd4575e4c8cd56
Keerthivasan-895 <98251510+Keerthivasan-895@users.noreply.github.com>
2022-02-06 13:09:55 +0530

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,14 @@
+package assignment3.week3;
+
+public class AxisBank {
+	
+	public static void main(String[] args) {
+		AxisBank a=new AxisBank();
+		a.deposit();
+	}
+	
+	public static void deposit() {
+		System.out.println("Axis Bank amount is: 10000");
+	}
+
+}
---
+++ BankInfo.java
@@ -0,0 +1,14 @@
+package assignment3.week3;
+
+public class BankInfo {
+	
+	public static void saving() {
+		System.out.println("Savings Account");
+	}
+public void fixed() {
+	System.out.println("Fixed Interest");
+}
+public static void deposit() {
+	System.out.println("The Deposit Amount is: 50000");
+}
+}
